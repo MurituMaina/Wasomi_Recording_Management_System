769f5face11f47d47669f6d9bba36afd72a8b302
MurituMaina <vincent.maina@student.moringaschool.com>
2022-11-10 11:49:51 +0300

    add the form to Signup js file

--- src/navbar components/signup/Signup.js
+++ src/navbar components/signup/Signup.js
@@ -1,11 +1,31 @@
-import "./signup.css"
+import "./signup.css";
 
 const Signup = () => {
-    return ( 
-        <h1>
-            hello
-        </h1>
-     );
-}
- 
+//   return <h1>hello</h1>;
+ return (
+
+   <div className="container">
+     <div className="col-md-6 col-md-offset-3">
+       <h2 className="text-center">Sign Up</h2>
+
+       <form 
+    //    onSubmit={this.props.handleSubmit(this.handleFormSubmit)}
+       >
+         <input name="email" type="text" label="Email" />
+         <input name="password" type="password" label="Password" />
+         <input
+           name="passwordConfirmation"
+           type="password"
+           label="Password Confirmation"
+         />
+
+         <button action="submit" className="btn btn-primary">
+           Sign up
+         </button>
+       </form>
+     </div>
+   </div>
+ );
+};
+
 export default Signup;
