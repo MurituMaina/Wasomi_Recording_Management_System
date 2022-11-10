ed0454d687ae6cc9a4b01e44e395545001bf9aea
MurituMaina <vincent.maina@student.moringaschool.com>
2022-11-10 12:06:52 +0300

    order the navbar link for signup and add form to sign up

--- src/navbar components/navbar/Navbar.js
+++ src/navbar components/navbar/Navbar.js
@@ -1,44 +1,31 @@
-import "./navbar.css"
+import "./navbar.css";
 import Home from "../home/Home";
 import MyBook from "../MyBooks/MyBook";
 import BookCategories from "../BookCategories/BookCategories";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Signup from "../signup/Signup";
 
-const Navbar = () =>{
+const Navbar = () => {
+  return (
+    <Router>
+      <nav className="navbar">
+        <div>
+          <Link to="/">HOME</Link>
+          <Link to="/bookCategories">BOOK CATEGORIES</Link>
 
-    
+          <Link to="/myBook">MY BOOKS</Link>
+          <Link to="/signUp">SIGN UP</Link>
+        </div>
+      </nav>
 
-    return(
-     
-        <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/myBook" element={<MyBook />} />
+        <Route path="/bookCategories" element={<BookCategories />} />
+        <Route path="/signUp" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+};
 
-<nav className="navbar">
-            <div>
-                    <Link to="/">HOME</Link>
-                    <Link to="/bookCategories">BOOK CATEGORIES</Link>
-                    <Link to="/signUp">SIGN UP</Link>
-                    <Link to="/myBook">MY BOOKS</Link>
-            </div>
-        </nav>
-
-              <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/myBook" element={<MyBook />} />
-                <Route path="/signUp" element={<Signup/>} />
-          <Route path="/bookCategories" element={<BookCategories />} />
-        </Routes>
-        </Router>
-
-
-     );
-
-
-
-        
-    
-        
-
-}
-
-export default Navbar
+export default Navbar;
--- src/navbar components/signup/Signup.js
+++ src/navbar components/signup/Signup.js
@@ -1,11 +1,31 @@
-import "./signup.css"
+import "./signup.css";
 
 const Signup = () => {
-    return ( 
-        <h1>
-            hello
-        </h1>
-     );
-}
- 
+  return (
+       <div className="container">
+      <div className="col-md-6 col-md-offset-3">
+        <h2 className="text-center">Sign Up</h2>
+
+        <form>
+          <input name="email" type="text" label="Email" />
+          <br />
+          <input name="password" type="password" label="Password" />
+          <br />
+          <input
+            name="passwordConfirmation"
+            type="password"
+            label="Password Confirmation"
+          />
+          <br />
+
+          <button action="submit" className="btn btn-primary">
+            Sign up
+          </button>
+        </form>
+      </div>
+      <br />
+    </div>
+  );
+};
+
 export default Signup;
